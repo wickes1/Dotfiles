e33e014bda74c0a6fd5ad8a279e2d3db9e6c77b0
Wickes1 <27881570+wickes1@users.noreply.github.com>
2023-07-27 16:46:18 -0400

    remove portainer 8000 port

--- install-dev-tools.sh
+++ install-dev-tools.sh
@@ -24,7 +24,8 @@ sudo groupadd -f docker
 sudo usermod -aG docker $USER
 newgrp docker
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+# docker container rm -f portainer
 
 # Setup Git
 # git config --global user.name ""
