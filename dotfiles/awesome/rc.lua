c1f6a5c9d9aec7b185e798b0df92b024701e6943
Wickes1 <27881570+wickes1@users.noreply.github.com>
2023-07-28 14:52:31 -0400

    fix xfce4 power manager override awesomewm keybind

--- dotfiles/awesome/rc.lua
+++ dotfiles/awesome/rc.lua
@@ -84,19 +84,19 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- Table of layouts to cover with awful.layout.inc, order matters.
 tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
-        -- awful.layout.suit.floating,
         awful.layout.suit.tile,
-        awful.layout.suit.tile.left,
-        awful.layout.suit.tile.bottom,
-        awful.layout.suit.tile.top,
-        awful.layout.suit.fair,
-        awful.layout.suit.fair.horizontal,
-        awful.layout.suit.spiral,
-        awful.layout.suit.spiral.dwindle,
-        awful.layout.suit.max,
-        awful.layout.suit.max.fullscreen,
-        awful.layout.suit.magnifier,
-        awful.layout.suit.corner.nw,
+        awful.layout.suit.floating,
+        -- awful.layout.suit.tile.left,
+        -- awful.layout.suit.tile.bottom,
+        -- awful.layout.suit.tile.top,
+        -- awful.layout.suit.fair,
+        -- awful.layout.suit.fair.horizontal,
+        -- awful.layout.suit.spiral,
+        -- awful.layout.suit.spiral.dwindle,
+        -- awful.layout.suit.max,
+        -- awful.layout.suit.max.fullscreen,
+        -- awful.layout.suit.magnifier,
+        -- awful.layout.suit.corner.nw,
     })
 end)
 -- }}}
@@ -673,7 +673,7 @@ beautiful.notification_fg = '#d4be98'
 -- awful.spawn.with_shell("$HOME/.screenlayout/default.sh")
 -- awful.spawn.with_shell("picom")
 -- awful.spawn.with_shell("$HOME/.config/awesome/scripts/lock-screen.sh")
--- awful.spawn.with_shell("xfce4-power-manager")
+awful.spawn.with_shell("xfce4-power-manager")
 -- awful.spawn.with_shell("fcitx5")
 -- awful.spawn.with_shell("flameshot")
 -- awful.spawn.with_shell("nm-applet")
