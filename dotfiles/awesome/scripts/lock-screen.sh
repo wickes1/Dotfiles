f1c1944e7812a830fd1df82bed82416f214d060e
Wickes1 <27881570+wickes1@users.noreply.github.com>
2023-09-13 17:22:07 -0400

    fix xidlehook

--- dotfiles/awesome/scripts/lock-screen.sh
+++ dotfiles/awesome/scripts/lock-screen.sh
@@ -2,23 +2,43 @@
 
 # Only exported variables can be used within the timer's command.
 export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
-export BRIGHTNESS_VALUE="$(ddcutil getvcp 10 | awk '/Brightness/{print $9}' | sed 's/,//g')"
 
 # Run xidlehook
 xidlehook \
-  `# Don't lock when there's a fullscreen application` \
-  --not-when-fullscreen \
-  `# Don't lock when there's audio playing` \
-  --not-when-audio \
-  `# Dim the screen after 60 seconds, undim if user becomes active` \
-  --timer 60 \
-    'ddcutil setvcp 10 0' \
-    'ddcutil setvcp 10 $BRIGHTNESS_VALUE --sleep-multiplier .1' \
-  `# Undim & lock after 10 more seconds` \
-  --timer 10 \
+    `# Don't lock when there's a fullscreen application` \
+    --not-when-fullscreen \
+    `# Don't lock when there's audio playing` \
+    --not-when-audio \
+    `# Dim the screen after 60 seconds, undim if user becomes active` \
+    --timer 120 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+    `# Undim & lock after 10 more seconds` \
+    --timer 10 \
     'betterlockscreen -l dim' \
-    'ddcutil setvcp 10 $BRIGHTNESS_VALUE --sleep-multiplier .1' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 300 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+    `# Finally, suspend an hour after it locks` \
+    --timer 300 \
     'systemctl suspend' \
-    'ddcutil setvcp 10 $BRIGHTNESS_VALUE --sleep-multiplier .1'
+    'openrgb -p sakura; xrandr --output "$PRIMARY_DISPLAY" --brightness 1'
+
+# For external monitor
+# export BRIGHTNESS_VALUE="$(ddcutil getvcp 10 | awk '/Brightness/{print $9}' | sed 's/,//g')"
+# # Run xidlehook
+# xidlehook \
+#     `# Don't lock when there's a fullscreen application` \
+#     --not-when-fullscreen \
+#     `# Don't lock when there's audio playing` \
+#     --not-when-audio \
+#     `# Dim the screen after 60 seconds, undim if user becomes active` \
+#     --timer 10 \
+#     'ddcutil setvcp 10 0' \
+#     'ddcutil setvcp 10 $BRIGHTNESS_VALUE --sleep-multiplier 1' \
+#     `# Undim & lock after 10 more seconds` \
+#     --timer 10 \
+#     'betterlockscreen -l dim' \
+#     'ddcutil setvcp 10 $BRIGHTNESS_VALUE --sleep-multiplier 1' \
+#     `# Finally, suspend an hour after it locks`
+#     --timer 10 \
+#     'systemctl suspend' \
+#     'ddcutil setvcp 10 $BRIGHTNESS_VALUE --sleep-multiplier 1; openrgb -p sakura'
